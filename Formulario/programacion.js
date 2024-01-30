74cbcebeed1b2849cfbf075b2217a65ab8b973ab
Pablo <pablo_agustin87@hotmail.com>
2024-01-29 22:36:14 -0300

    este es mi primer commit 2024

---
+++ Formulario/programacion.js
@@ -0,0 +1,12 @@
+        var nombre = document.getElementById('nombre');
+        var apellido = document.getElementById('apellido');
+        var dni = document.getElementById('dni');
+        var telefono = document.getElementById('telefono');
+      
+        var form = document.getElementById("formulario");
+            form.addEventListener('submit', function(event){
+                
+                
+        });
+            
+    
